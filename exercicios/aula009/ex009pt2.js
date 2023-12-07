e2755b46286ec6c29d880bc78af471d10ae648dd
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2023-12-07 16:02:29 -0300

    adicionei comandos

--- exercicios/aula009/ex009pt2.js
+++ exercicios/aula009/ex009pt2.js
@@ -1,2 +1,11 @@
-var hora = 8
+var hora = 22
 console.log(`Agora são exatamente ${hora} horas.`)
+if(hora < 5){
+    console.log('Bom Madrugada')
+} else if(hora < 12){
+    console.log('Bom dia')
+} else if(hora < 18){
+    console.log('Boa Tarde')
+} else if(hora < 23){
+    console.log('Boa Noite')
+}
