4ca22c53f56b83c70a3e266c73653b65a9362c37
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2023-12-11 16:36:35 -0300

    arrumei background dos periodos do dia

--- exercicios.praticados/aula09/ex02/script.js
+++ exercicios.praticados/aula09/ex02/script.js
@@ -2,17 +2,20 @@ function carregar(){
     var msg = window.document.getElementById('msg')
      var img = window.document.getElementById('imagem')
      var data = new Date()
-     //var hora = data.getHours()
-     var hora = 22
+     var hora = data.getHours()
      msg.innerHTML = `Agora são ${hora} horas`
      if (hora >= 0 && hora < 12) {
         // BOM DIA!
         img.src = 'imagens/dia.jpg'
+        document.body.style.background = '#EFD081'
+
      } else if(hora >= 12 && hora < 18) {
         //BOA TARDE!
         img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#ECA04F'
      } else {
         //BOA NOITE
         img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#301E35'
      }
 }
