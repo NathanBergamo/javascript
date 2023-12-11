2d6a74466863e7e1325eca813240b015ed09ed3d
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2023-12-11 16:30:53 -0300

    arrumei as horas

--- exercicios.praticados/aula09/ex02/script.js
+++ exercicios.praticados/aula09/ex02/script.js
@@ -2,13 +2,17 @@ function carregar(){
     var msg = window.document.getElementById('msg')
      var img = window.document.getElementById('imagem')
      var data = new Date()
-     var hora = data.getHours()
+     //var hora = data.getHours()
+     var hora = 22
      msg.innerHTML = `Agora são ${hora} horas`
-     if(hora >= 0 && hora < 12){
+     if (hora >= 0 && hora < 12) {
         // BOM DIA!
-     } else if(hora >= 12 && hora < 18){
+        img.src = 'imagens/dia.jpg'
+     } else if(hora >= 12 && hora < 18) {
         //BOA TARDE!
+        img.src = 'imagens/tarde.jpg'
      } else {
         //BOA NOITE
+        img.src = 'imagens/noite.jpg'
      }
 }
