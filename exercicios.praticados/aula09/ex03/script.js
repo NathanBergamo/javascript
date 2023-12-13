897d32bd256053636fca5fc8573745d43494df48
Nathan Bergamo <nathanbergamoprogrammer@gmail.com>
2023-12-13 14:41:42 -0300

    adicionei etapas de idade

--- exercicios.praticados/aula09/ex03/script.js
+++ exercicios.praticados/aula09/ex03/script.js
@@ -9,11 +9,40 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             gênero = 'Homem'
+            if (idade >= 0 && idade < 7){
+                //crianca
+            } else if(idade >=7 && idade < 15){
+                //adolescente
+            } else if(idade >=15 && idade <21){
+                //jovem
+            } else if(idade >=21 && idade <35){
+                //adulto
+            } else if(idade >= 35 && idade <60){
+                //ainda aduto
+            } else if(idade >= 60 && idade <90){
+                //idoso
+            }
         } else if (fsex[1].checked){
             gênero = 'Mulher'
+            if (idade >= 0 && idade < 7){
+                //crianca
+            } else if(idade >=7 && idade < 15){
+                //adolescente
+            } else if(idade >=15 && idade <21){
+                //jovem
+            } else if(idade >=21 && idade <35){
+                //adulto
+            } else if(idade >= 35 && idade <60){
+                //ainda aduto
+            } else if(idade >= 60 && idade <90){
+                //idoso
+            }
         }
+        res.style.textAlign = 'Center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
     } 
 }
